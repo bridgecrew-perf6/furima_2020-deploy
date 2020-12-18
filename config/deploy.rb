10c07810bbf5af574309ae91d866aec0e1e83135
usagrammer <uny0102@gmail.com>
2020-12-18 14:38:46 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'furima_2020-hoge'
+set :application, 'furima_2020-deploy'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:usagrammer/furima_2020-deploy.git'
