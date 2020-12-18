698d9202d6735e4bd533fb8dcc4aa96fa2aefa5f
usagrammer <uny0102@gmail.com>
2020-12-18 15:51:01 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  include ActiveSupport::LoggerSilence
-  
+  # include ActiveSupport::LoggerSilence
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
